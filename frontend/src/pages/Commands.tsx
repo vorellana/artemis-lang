086ccf5040fecc37619065ee48b9feb0a1ebaee0
victor <victor.orellana@2bak.cl>
2023-04-20 18:51:23 -0500

    Deploy review 2

--- frontend/src/pages/Commands.tsx
+++ frontend/src/pages/Commands.tsx
@@ -48,7 +48,7 @@ function Commands() {
 
   const sendData = async () => {
 
-    console.log('variables 546');
+    console.log('variables 624');
     console.log('NEXT_PUBLIC_API_URL -> ', process.env.NEXT_PUBLIC_API_URL);
     console.log('NEXT_PUBLIC_HOSTNAME -> ', process.env.NEXT_PUBLIC_HOSTNAME);
     console.log('NEXT_PUBLIC_API_KEY -> ', process.env.NEXT_PUBLIC_API_KEY);
