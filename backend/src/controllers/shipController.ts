95195f8e2ac13a07920b97f52cdc22b4ad308493
victor <victor.orellana@2bak.cl>
2023-04-21 03:36:44 -0500

    feat(back): add security

--- backend/src/controllers/shipController.ts
+++ backend/src/controllers/shipController.ts
@@ -1,9 +1,19 @@
 import { Request, Response } from "express";
 import { shipService } from "../services/shipService";
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const apiKey = process.env.API_KEY || '-';
 
 export const shipController = {
   evaluate: async (req: Request, res: Response) => {
     const expression = req.body;
+    const reqApiKey = req.headers['x-api-key'] as string || '';
+
+    if (reqApiKey !== apiKey) {
+      return res.status(401).json({ error: "Unauthorized" });
+    }
 
     try {
       const result = await shipService.evaluate(expression);
@@ -11,5 +21,6 @@ export const shipController = {
     } catch (error) {
       res.status(400).json({ error: error.message });
     }
+
   },
 };
