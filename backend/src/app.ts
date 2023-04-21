136453010959173a66f90c91eef44d8e02c956c7
victor <victor.orellana@2bak.cl>
2023-04-21 04:36:02 -0500

    Deploy review 8

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -12,6 +12,12 @@ const corsOptions = {
 };
 
 app.use(cors(corsOptions));
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, x-api-key");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS, HEAD");
+  next();
+});
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use("/ship", shipRouter);
