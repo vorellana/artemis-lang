c537cb95b476b060de3176c4de095966865145a3
victor <victor.orellana@2bak.cl>
2023-04-21 04:08:03 -0500

    Deploy review 7

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -5,25 +5,15 @@ import { shipRouter } from "./routes/shipRoutes";
 
 const app: Application = express();
 
-// const corsOptions = {
-//   origin: '*',
-//   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-//   allowedHeaders: ['Content-Type', 'Authorization']
-// };
-
 const corsOptions = {
   origin: 'http://artemis-frontend-balance-b-1639758126.us-east-1.elb.amazonaws.com',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'HEAD'],
-  optionsSuccessStatus: 200
+  allowedHeaders: ['Content-Type', 'Authorization', 'x-api-key'],
 };
-
-app.use(cors(corsOptions));
-
 
 app.use(cors(corsOptions));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-
 app.use("/ship", shipRouter);
 
 export default app;
